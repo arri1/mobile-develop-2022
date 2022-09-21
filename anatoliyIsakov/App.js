b3280b8e2b5b11ec2472de1cf10e4565c72aa6ee
Anatoliy1999 <anatoliy.isakov.1999@gmail.com>
2022-09-21 20:54:49 +0900

    'lab_2'

--- anatoliyIsakov/App.js
+++ anatoliyIsakov/App.js
@@ -1,117 +1,94 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from "react";
+import {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [value, setValue] = useState(1)
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity  
+          style={styles.button}
+          onPress={() => {
+            setValue(1)
+          }}
+        >
+          <Text style={styles.text}>1</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity  
+          style={styles.button}
+          onPress={() => {
+            setValue(2)
+          }}
+        >
+          <Text style={styles.text}>2</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity  
+          style={styles.button}
+          onPress={() => {
+            setValue(3)
+          }}
+        >
+          <Text style={styles.text}>3</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity  
+          style={styles.button}
+          onPress={() => {
+            setValue(4)
+          }}
+        >
+          <Text style={styles.text}>4</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.tab}>
+        <Text style={styles.text}>
+          {value}
+        </Text>
+      </View>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: "column",
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  button: {
+    alignItems: "center",
+    backgroundColor: "skyblue",
+    margin: 10,
+    flex: 1,
+    height: 50,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  tab: {
+    alignItems: "center",
+    backgroundColor: "coral",
+    margin: 10,
+    height: 50,
+    width: 350,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  text: {
+    fontSize: 20,
+    fontWeight: "500",
+  }
+})
 
 export default App;
