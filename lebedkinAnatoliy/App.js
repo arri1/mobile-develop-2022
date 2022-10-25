e430f8b4aab5571aeb0213fcc4e4565727fbc210
navaroe <eizelthorn@gmail.com>
2022-10-25 17:50:17 +0900

    Lab 3 work-in-progress

--- lebedkinAnatoliy/App.js
+++ lebedkinAnatoliy/App.js
@@ -4,6 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {LabOne} from './screens/LabOne';
 import {LabTwo} from './screens/LabTwo';
+import {LabThree} from './screens/LabThree';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
@@ -26,6 +27,17 @@ const App = () => {
         <Tab.Screen
           name="Lab2"
           component={LabTwo}
+          options={{
+            tabBarIcon: () => {
+              return (
+                <Icon name="list" size={20} color="#009" />
+              );
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Lab3"
+          component={LabThree}
           options={{
             tabBarIcon: () => {
               return (
