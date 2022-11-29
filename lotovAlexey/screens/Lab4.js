228bed0bed7ae8f419b96919403f5b4609344b6f
Alexey670412 <91104532+Alexey670412@users.noreply.github.com>
2022-11-29 17:32:31 +0900

    lab4 make beautifully

--- lotovAlexey/screens/Lab4.js
+++ lotovAlexey/screens/Lab4.js
@@ -1,5 +1,4 @@
-import {useState} from 'react';
-import {View, Button} from 'react-native';
+import {View, Button, StyleSheet, TouchableOpacity, Text} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {setSkyColor} from '../src/redux/actions';
 
@@ -21,10 +20,28 @@ const Lab4 = () => {
         backgroundColor: '#8fcbbc',
         justifyContent: 'center',
       }}>
-      <Button title="Night" onPress={setColorNight} />
-      <Button title="Day" onPress={setColorDay} />
+      <TouchableOpacity style={styles.button} onPress={setColorDay}>
+        <Text>Day</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={setColorNight}>
+        <Text>Hight</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    width: 100,
+    height: 50,
+    backgroundColor: '#0080FF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+    elevation: 3,
+    borderRadius: 15,
+  },
+});
 
 export default Lab4;
