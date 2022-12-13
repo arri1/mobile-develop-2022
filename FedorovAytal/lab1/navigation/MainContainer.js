3191a1f4edcb2aa989a4d866048fd393add31b3a
fedorov aytal <fedorovaytal@mail.ru>
2022-12-13 16:48:54 +0900

    lab2

---
+++ FedorovAytal/lab1/navigation/MainContainer.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+import Lab1Screen from './screens/Lab1Screen';
+import Lab2Screen from './screens/Lab2Screen';
+import Lab3Screen from './screens/Lab3Screen';
+
+const lab1Name = 'lab1';
+const lab2Name = 'lab2';
+const lab3Name = 'lab3';
+
+const Tab = createBottomTabNavigator();
+
+
+export default function MainContainer(){
+    return(
+        <NavigationContainer>
+            <Tab.Navigator
+                initialRouteName={lab1Name}
+                screenOptions={({route}) => ({
+                    tabBarIcon: ({focused, color, size}) => {
+                        let iconName;
+                        let rn = route.name;
+
+                        if (rn === lab1Name, lab2Name, lab3Name) {
+                            iconName = focused ? 'list' : 'list-outline';
+                        }
+
+                        return <Ionicons name={iconName} size={size} color={color}/>
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: 'tomato',
+                    inactiveTintColor: 'grey',
+                    labelStyle: { paddingBottom: 10, fontSize: 10},
+                    style: {padding: 10, height: 70}
+                }}
+                
+                
+                >
+                
+                <Tab.Screen name={lab1Name} component={Lab1Screen}/>
+                <Tab.Screen name={lab2Name} component={Lab2Screen}/>
+                <Tab.Screen name={lab3Name} component={Lab3Screen}/>
+
+
+
+            </Tab.Navigator>
+
+        </NavigationContainer>
+    );
+}
