b14c2cb18732500893a34a48f6083a601ffd87a4
navaroe <eizelthorn@gmail.com>
2022-12-20 18:11:26 +0900

    Added redux counter to Lab 2

--- lebedkinAnatoliy/screens/LabTwo.js
+++ lebedkinAnatoliy/screens/LabTwo.js
@@ -2,10 +2,12 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {View, Text} from 'react-native';
+import {useSelector} from 'react-redux';
 
 export const LabTwo = () => {
     const [name, setName] = useState();
     const [email, setEmail] = useState();
+    const count = useSelector(state => state.counter.value);
     useEffect(() => {
         axios
             .get('https://reqres.in/api/users/1')
@@ -18,6 +20,7 @@ export const LabTwo = () => {
     return (
         <View>
             <Text>Full Name:{name}, Email:{email}</Text>
+            <Text>{count}</Text>
         </View>
     );
 };
