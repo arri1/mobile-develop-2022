1a445deba505bedfc7eff21c3385ff8c5b2f571c
Ilya Ivanov <iljke@inbox.ru>
2023-06-08 19:54:15 +0900

    fix lab1

--- ilyaIvanov/App.js
+++ ilyaIvanov/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NavigationContainer } from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
 import BottomTab from './components/bottom-tab';
 
 function App() {
-  return <SafeAreaProvider style={styles.container}>
-    <NavigationContainer>
-      <BottomTab>
-      </BottomTab>
-    </NavigationContainer>
-  </SafeAreaProvider>
+  return (
+    <SafeAreaProvider style={styles.container}>
+      <NavigationContainer>
+        <BottomTab></BottomTab>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:'#fff'
-  }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
 
 export default App;
--- ilyaIvanov/components/bottom-tab.js
+++ ilyaIvanov/components/bottom-tab.js
@@ -1,63 +1,115 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { View, Text, Image } from 'react-native';
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {View, Text, Image} from 'react-native';
 import HomeIcon from '../assets/icons/home.png';
 import SearchIcon from '../assets/icons/search.png';
 import NotificationIcon from '../assets/icons/messenger.png';
 import PostIcon from '../assets/icons/play.png';
 import ProfileIcon from '../assets/icons/user.png';
 
-import { LabOne, lab1 } from '../src/screens/lab1';
-
+import {LabOne, lab1} from '../src/screens/lab1';
 
 const Tab = createBottomTabNavigator();
 
 const Home = () => {
-  return <View style={{backgroundColor:'#ccc', justifyContent:'center', flex:1, alignItems:'center'}}>
-    <Text style={{fontSize:25}}>Home</Text>
-  </View>
-}
+  return (
+    <View
+      style={{
+        backgroundColor: '#ccc',
+        justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center',
+      }}>
+      <Text style={{fontSize: 25}}>Home</Text>
+    </View>
+  );
+};
 
 const Search = () => {
-  return <View style={{backgroundColor:'#ccc', justifyContent:'center', flex:1, alignItems:'center'}}>
-    <Text style={{fontSize:25}}>Search</Text>
-  </View>
-}
+  return (
+    <View
+      style={{
+        backgroundColor: '#ccc',
+        justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center',
+      }}>
+      <Text style={{fontSize: 25}}>Search</Text>
+    </View>
+  );
+};
 
 const Post = () => {
-  return <View style={{backgroundColor:'#ccc', justifyContent:'center', flex:1, alignItems:'center'}}>
-    <Text style={{fontSize:25}}>Post</Text>
-  </View>
-}
+  return (
+    <View
+      style={{
+        backgroundColor: '#ccc',
+        justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center',
+      }}>
+      <Text style={{fontSize: 25}}>Post</Text>
+    </View>
+  );
+};
 
 const Profile = () => {
-  return <View style={{backgroundColor:'#ccc', justifyContent:'center', flex:1, alignItems:'center'}}>
-    <Text style={{fontSize:25}}>Profile</Text>
-  </View>
-}
+  return (
+    <View
+      style={{
+        backgroundColor: '#ccc',
+        justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center',
+      }}>
+      <Text style={{fontSize: 25}}>Profile</Text>
+    </View>
+  );
+};
 
 const Notifications = () => {
-  return <View style={{backgroundColor:'#ccc', justifyContent:'center', flex:1, alignItems:'center'}}>
-    <Text style={{fontSize:25}}>Notifications</Text>
-  </View>
-}
+  return (
+    <View
+      style={{
+        backgroundColor: '#ccc',
+        justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center',
+      }}>
+      <Text style={{fontSize: 25}}>Notifications</Text>
+    </View>
+  );
+};
 
-const BottomTab =() => {
-  return <Tab.Navigator
+const BottomTab = () => {
+  return (
+    <Tab.Navigator
       initialRouteName="Home"
       screenOptions={{
-        tabBarShowLabel:false,
-        tabBarActiveTintColor: '#e91e63', tabBarStyle:{height:60,position:'absolute', bottom:20,borderRadius:90,marginHorizontal:25}
-      }}
-    >
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#e91e63',
+        tabBarStyle: {
+          height: 60,
+          position: 'absolute',
+          bottom: 20,
+          borderRadius: 90,
+          marginHorizontal: 25,
+        },
+      }}>
       <Tab.Screen
         name="Home"
         component={LabOne}
         options={{
           tabBarLabel: 'Home',
-          tabBarIcon: ({ focused }) => (
-             <Image source={HomeIcon} style={{width:35,height:35,tintColor : focused ? "#000":"#333"}}/>
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={HomeIcon}
+              style={{
+                width: 35,
+                height: 35,
+                tintColor: focused ? '#000' : '#333',
+              }}
+            />
           ),
         }}
       />
@@ -67,8 +119,15 @@ const BottomTab =() => {
         component={Search}
         options={{
           tabBarLabel: 'Search',
-          tabBarIcon: ({ focused }) => (
-             <Image source={SearchIcon} style={{width:35,height:35,tintColor : focused ? "#000":"#333"}}/>
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={SearchIcon}
+              style={{
+                width: 35,
+                height: 35,
+                tintColor: focused ? '#000' : '#333',
+              }}
+            />
           ),
         }}
       />
@@ -78,8 +137,15 @@ const BottomTab =() => {
         component={Post}
         options={{
           tabBarLabel: 'Post',
-          tabBarIcon: ({ focused }) => (
-             <Image source={PostIcon} style={{width:35,height:35,tintColor : focused ? "#000":"#333"}}/>
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={PostIcon}
+              style={{
+                width: 35,
+                height: 35,
+                tintColor: focused ? '#000' : '#333',
+              }}
+            />
           ),
         }}
       />
@@ -89,8 +155,15 @@ const BottomTab =() => {
         component={Notifications}
         options={{
           tabBarLabel: 'Notifications',
-          tabBarIcon: ({ focused }) => (
-             <Image source={NotificationIcon} style={{width:35,height:35,tintColor : focused ? "#000":"#333"}}/>
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={NotificationIcon}
+              style={{
+                width: 35,
+                height: 35,
+                tintColor: focused ? '#000' : '#333',
+              }}
+            />
           ),
         }}
       />
@@ -100,13 +173,20 @@ const BottomTab =() => {
         component={Profile}
         options={{
           tabBarLabel: 'Profile',
-          tabBarIcon: ({ focused }) => (
-             <Image source={ProfileIcon} style={{width:35,height:35,tintColor : focused ? "#000":"#333"}}/>
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={ProfileIcon}
+              style={{
+                width: 35,
+                height: 35,
+                tintColor: focused ? '#000' : '#333',
+              }}
+            />
           ),
         }}
       />
     </Tab.Navigator>
-
-}
+  );
+};
 
 export default BottomTab;
