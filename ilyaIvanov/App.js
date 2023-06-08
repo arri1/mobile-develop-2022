d5b8d618dfacc0c7dca8bc6322f4024400cc5c7a
Ilya Ivanov <iljke@inbox.ru>
2023-06-08 20:29:02 +0900

    fix lab 1

--- ilyaIvanov/App.js
+++ ilyaIvanov/App.js
@@ -1,24 +1,7 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {NavigationContainer} from '@react-navigation/native';
-import BottomTab from './components/bottom-tab';
+import React, {useEffect, useState} from 'react';
+import {StatusBar, View} from 'react-native';
+import {Navigation} from './screens/Navigation';
 
-function App() {
-  return (
-    <SafeAreaProvider style={styles.container}>
-      <NavigationContainer>
-        <BottomTab></BottomTab>
-      </NavigationContainer>
-    </SafeAreaProvider>
-  );
+export default function App() {
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
-
-export default App;
